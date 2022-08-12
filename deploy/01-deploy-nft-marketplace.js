73d470aa4a45a16f9d3c697163e9e56cb9e9cb06
pajserb0y <65039799+pajserb0y@users.noreply.github.com>
2022-08-12 21:01:49 +0200

    deploy scripts

---
+++ deploy/01-deploy-nft-marketplace.js
@@ -0,0 +1,23 @@
+const { deployments, network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../helper-functions")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deployer } = await getNamedAccounts()
+    const { log, deploy } = deployments
+
+    const nftMarketplace = await deploy("NftMarketplace", {
+        from: deployer,
+        args: [],
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, args)
+    }
+    log("-------------------------------------------")
+}
+
+module.exports.tags = ["all", "nftmarketplace"]
